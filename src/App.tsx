50a142ec601a087390c10dfd4cd2e8628a2f5f54
AbhishekDevJr <abhishek007coc@gmail.com>
2023-12-15 12:06:56 +0530

    Added Device Authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainComp from './components/MainComp';
 import './App.scss';
 import ReactGA from 'react-ga';
 
-function App() {  
+const ACCESS_TOKEN = localStorage.getItem('accessToken');
+
+function App() {
   const TRACKING_ID = "UA-258658703-2"; // OUR_TRACKING_ID
+  console.log('userAccessToken----------->', ACCESS_TOKEN);
   ReactGA.initialize(TRACKING_ID);
   return (
     <div className="App">
-      <MainComp />
+      {ACCESS_TOKEN ? <MainComp /> : <h1 style = {{color : 'indigo'}}>You Don't have Permission to Access this page.</h1>}
     </div>
   );
 }
