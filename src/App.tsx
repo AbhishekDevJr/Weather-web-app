31779225c6cf7de5d02f25ffd2332c336af45b78
AbhishekDevJr <abhishek007coc@gmail.com>
2023-12-19 15:09:05 +0530

    IP Address API Call

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MainComp from './components/MainComp';
 import './App.scss';
 import ReactGA from 'react-ga';
+import axios from 'axios';
 
 const ACCESS_TOKEN = localStorage.getItem('accessToken');
 
@@ -9,9 +10,23 @@ function App() {
   const TRACKING_ID = "UA-258658703-2"; // OUR_TRACKING_ID
   console.log('userAccessToken----------->', ACCESS_TOKEN);
   ReactGA.initialize(TRACKING_ID);
+
+  const apiFunction = async () => {
+    const apiRes = await axios.get('http://localhost:3000');
+    console.log('API RES----------->', apiRes);
+  }
+
+  useEffect(() => {
+    try {
+      apiFunction();
+    } catch (e: any) {
+      console.log('Msg------>', e.message);
+    }
+  }, []);
+
   return (
     <div className="App">
-      {ACCESS_TOKEN ? <MainComp /> : <h1 style = {{color : 'indigo'}}>You Don't have Permission to Access this page.</h1>}
+      {ACCESS_TOKEN ? <MainComp /> : <h1 style={{ color: 'indigo' }}>You Don't have Permission to Access this page.</h1>}
     </div>
   );
 }
